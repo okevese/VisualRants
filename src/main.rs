9563c32dad8c35359d9fa0d945d5cd345167f47a
oke vese <okevese@gmail.com>
2018-11-21 18:55:04 +0100

    Got unfiltered links from devrant feed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate reqwest;
+extern crate select;
+
+use select::document::Document;
+use select::predicate::Name;
+
+
 fn main() {
-    println!("Hello, world!");
+    rant_feed("https://devrant.com/feed");
+}
+
+
+fn rant_feed(url: &str) {
+	let res = reqwest::get(url).unwrap();
+	assert!(res.status().is_success());
+
+	Document::from_read(res)
+		.unwrap()
+		.find(Name("a"))
+		.filter_map(|n| n.attr("href"))
+		.for_each(|x| println!("{:?}", x)); // TODO: filter for rant tags here <==
+
 }
