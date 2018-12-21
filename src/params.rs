cfd2c905eee24dff2d747219a28c586b739fd42c
oke vese <okevese@gmail.com>
2018-12-21 21:47:33 +0100

    changed query parameters to constants

--- src/params.rs
+++ src/params.rs
@@ -1,39 +1,20 @@
 
+#[derive(Debug)]
+pub enum Sort {}
 
-pub enum Sort {
-    Algo,
-    Top,
-    Recent,
-}
-
-
-pub enum Range {
-    Day,
-    Week,
-    Month,
-    All,
-}
-
-// To ensure only correct string values for `get_rants` parameters
 impl Sort {
-    pub fn as_str(&self) -> &str {
-        match self {
-            &Sort::Algo => "algo",
-            &Sort::Top => "top",
-            &Sort::Recent => "recent",
-        }
-    }
+    pub const RECENT: &'static str = "recent";
+    pub const TOP: &'static str = "top";
+    pub const ALGO: &'static str = "algo";
 }
 
+
+pub enum Range {}
 
 impl Range {
-    pub fn as_str(&self) -> &str {
-        match self {
-            &Range::Day => "day",
-            &Range::Week => "week",
-            &Range::Month => "month",
-            &Range::All => "all",
-        }
-    }
+    pub const DAY: &'static str = "day";
+    pub const WEEK: &'static str = "week";
+    pub const MONTH: &'static str = "month";
+    pub const ALL: &'static str = "all";
 }
 
