8ce84f39772c897b0187363487f75b7cf8dfdc6c
oke vese <okevese@gmail.com>
2018-12-21 21:49:22 +0100

    comparing inputs with constants to create query params

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,21 @@ use visual_rants::params::*;
 
 
 fn main() {
-
     
-    let algo = Sort::Algo;
-    let recent = Sort::Recent;
-    let top = Sort::Top;
-    let day = Range::Day;
-    let week = Range::Week;
-    let month = Range::Month;
-    let all = Range::All;
+    let algo = Sort::ALGO;
+    //let recent = Sort::RECENT;
+    let top = Sort::TOP;
+    let day = Range::DAY;
+    let week = Range::WEEK;
+    let month = Range::MONTH;
+    let all = Range::ALL;
     
 
 
     let limit: &str = "5";      // Number of rants to return
     let skip: &str = "0";       // Number of rants to skip
 
- 
+    
     #[derive(StructOpt, Debug)]
     #[structopt(name = "VisualRants", about = "See a visualization of rants.")]
     struct Cli {
@@ -35,11 +34,23 @@ fn main() {
         skip: String,
     }
 
-    let args = Cli::from_args();
-    println!("{:?}", args);
-    println!("{:?}", args.range);
 
-
+    impl Cli {
+        fn create_request_params() -> Cli {
+            let args = Cli::from_args();
+            match args.sort.as_ref() {
+                Sort::RECENT => {
+                    println!("YES, A MATCH! YAAAAY");
+                    args
+                },
+                _ => {
+                    println!("No match");
+                    args
+                }
+            }
+        }
+    }
+    println!("{:?}", Cli::create_request_params());
 
     match get_rants(algo, day, limit, skip) {
         Ok(rants) => plot(prepare_data(rants)),
